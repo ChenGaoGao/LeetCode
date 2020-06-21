47d67e63db2e34b111db6f5e3737844a760f209a
Oliver <923138040@qq.com>
2020-06-21 23:44:23 +0800

    207

--- LeetCode/Map/207 课程表.swift
+++ LeetCode/Map/207 课程表.swift
@@ -18,38 +18,91 @@ import Foundation
 class Solution207 {
     class func solution() {
         let numCourses = 6
-        let prerequisites = [[1,2], [1,4],
-                             [2,4], [2,3],
-                             [3,5], [4,5]]
+        let prerequisites: [[Int]] = [[2,1], [3,2],
+                                      [4,1], [4,2],
+                                      [5,3], [5,4]]
         
         let solution = Solution207()
-        print(solution.canFinish(numCourses, prerequisites))
+        print(solution.canFinish1(numCourses, prerequisites))
     }
     
     
     
     // 广度优先(Breadth-First-Search, 缩写为BFS)
     func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
-        // 入度
-        var ruduArray: [Int] = Array(repeating: 0, count: numCourses)
+        if numCourses <= 1 { return true }
         
-        // 统计每个节点的入度
+        var myNumCourses = numCourses
+        // 邻接表 将课程前置条件列表 转换 为 邻接表
+        var adjacency: [[Int]] = Array(repeating: [], count: myNumCourses)
+        // 入度
+        var indegrees: [Int] = Array(repeating: 0, count: myNumCourses)
+        
         for array in prerequisites {
-            ruduArray[array[0]] += 1
+            // 转化课程条件 为 邻接表
+            adjacency[array[1]].append(array[0])
+            // 统计每个节点的入度
+            indegrees[array[0]] += 1
+        }
+
+        // 广度队列
+        var queue: [Int] = []
+        for i in 0..<myNumCourses {
+            if indegrees[i] == 0 {
+                queue.append(i)
+            }
         }
         
-        print(ruduArray)
-        
-        
-        
-        
-        return false
+        while !queue.isEmpty {
+            let pre = queue.removeFirst()
+            myNumCourses -= 1
+            for item in adjacency[pre] {
+                indegrees[item] -= 1
+                if indegrees[item] == 0 {
+                    queue.append(item)
+                }
+            }
+        }
+
+        return myNumCourses == 0
     }
     
     
+    // 深度优先(Deep First Search, 缩写为DFS)
+    func canFinish1(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
+        if numCourses <= 1 { return true }
+        // 邻接表 将课程前置条件列表 转换 为 邻接表
+        var adjacency: [[Int]] = Array(repeating: [], count: numCourses)
+        // 标记 0: 未被 DFS 访问    -1: 已被其他节点启动的 DFS 访问   1: 已被当前节点启动的 DFS 访问
+        var flags: [Int] = Array(repeating: 0, count: numCourses)
+        
+        for array in prerequisites {
+            // 转化课程条件 为 邻接表
+            adjacency[array[1]].append(array[0])
+        }
+        
+        
+        for i in 0..<numCourses {
+            if !dfs(adjacency, &flags, i) {
+                return false
+            }
+        }
+
+        return true
+    }
     
-    
-    
-    
-    
+    func dfs(_ adjacency: [[Int]], _ flags:inout [Int], _ index: Int) -> Bool {
+        if flags[index] == 1 { return false }
+        if flags[index] == -1 { return true }
+        flags[index] = 1
+        
+        for item in adjacency[index] {
+            if !dfs(adjacency, &flags, item) {
+                return false
+            }
+        }
+        flags[index] = -1
+        
+        return true
+    }
 }
